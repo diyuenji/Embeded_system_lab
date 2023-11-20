021af3bc66f81be538214175b72e00d735f3dc48
diyuenji <112496492+diyuenji@users.noreply.github.com>
2023-11-20 11:24:29 +0700

    fix lab4

--- Lab4/main/main.c
+++ Lab4/main/main.c
@@ -1,82 +1,183 @@
 /*
- * Created by Nhom 6
-*/
-
+ * creator/date: HieuHuuNguyen_2013149/Nov.17.11
+ * course name/ID: Embedded Systems/CO3054/
+ */
 #include <stdio.h>
-#include <inttypes.h>
-#include "driver/gpio.h"
-#include "sdkconfig.h"
+
+// RTOS services
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "FreeRTOSConfig.h"
 #include "freertos/queue.h"
 
+// GPIO services
+#include "esp_system.h"
+#include "esp_log.h"
+// configuration
+#include "sdkconfig.h"
 
-#include "data_types.h"
+#include "queueConfig.h"
 
-QueueHandle_t queue=NULL;
+QueueHandle_t perform_set;
 
-void Reception_task(void *pvParameter)
+static uint16_t numAvalQueue;
+
+static const char *TAG = "LAB4";
+static char *god_lyrics[] = {
+    "Once you play God, once you play God",
+    "They're gonna crumble one by one",
+    "Then we gon' ride right into the sun",
+    "Like it's the day my kingdom come",
+    "Baby, we're",
+    "Go-Go-Go-Go-Go-Gods"};
+int length_lyrics=sizeof(god_lyrics) / sizeof(god_lyrics[0]);
+lyric_t create_lyric(uint32_t input_index, char *input_content)
 {
-    Task* taskreceive = (Task*)pvParameter;
-    //classify
-    
-     for(int i=0;i<sizeof(taskreceive)/sizeof(taskreceive[0]);i++){
-        taskreceive[i].dataType =taskreceive[i].ID%3;
-    }
+    lyric_t *self = malloc(sizeof(lyric_t));
+    self->index = input_index;
+    self->content = input_content;
+    return *self;
+}
+// Just print some information
+void set_up()
+{
+    printf("********************************\n");
+    printf("* Queue length: %d\n", __MAXLEN__);
+    printf("* Max ID number: %d\n", __NOISE_VOLUME__);
+    printf("********************************\n");
+}
 
-    
-    // push to queue
-    for(int i=0;i<sizeof(taskreceive)/sizeof(taskreceive[0]);i++){
-        if (xQueueSend(queue, taskreceive[i], QUEUE_SEND_WAITS) != pdTRUE)
-        {
-            printf("Failed to send %d to queue ! \n", taskreceive[i].ID);
-        }
-        else printf("gg");
+// this function send them to a queue
+void send_lyric(QueueHandle_t queue, lyric_t self)
+{
+    if (self.index < 100)
+    {
+        ESP_LOGI(TAG, "[Send] at %ldms, Send item to queue, ID: %ld, Topic: \"%s\"", pdTICKS_TO_MS(xTaskGetTickCount()), self.index, self.content);
+        // printf("* at %ldms, NewJeans want to sing the %ld sentence.\n* %s\n", pdTICKS_TO_MS(xTaskGetTickCount()), self.index, self.content);
     }
+    else
+    {
+        ESP_LOGI(TAG, "[Send] at %ldms, [Noise item], ID: %ld, Topic: \"%s\"", pdTICKS_TO_MS(xTaskGetTickCount()), self.index, self.content);
+        // printf("* at %ldms, Noise voice, the %ld sentence.\n* %s\n", pdTICKS_TO_MS(xTaskGetTickCount()), self.index, self.content);
+    }
+    if (xQueueSend(queue, (void *)&self, (TickType_t)0) != pdTRUE)
+    {
+        ESP_LOGI(TAG, "[Queue] Unsuccessful! No slot availiable in queue, queue is full");
+        printf("* at %ldms, NewJeans do not want to sing anymore, they are \"OVERLOADED\"\n", pdTICKS_TO_MS(xTaskGetTickCount()));
+    }
+    else
+    {
+        numAvalQueue =  uxQueueSpacesAvailable(perform_set);
+        ESP_LOGI(TAG, "[Queue] Successful! %d/%d slot left in queue", numAvalQueue, __MAXLEN__);
+    }
+    printf("\n");
+}
 
-    while(1){
-        vTaskDelay(pdMS_TO_TICKS(QUEUE_SEND_DELAY));
+// this function presents the reception task
+// In this lab, this function is distributed lyrics to sing
+void distribute_lyrics(void *pvParameter)
+{
+    // Create queue
+    perform_set = xQueueCreate(__MAXLEN__, sizeof(lyric_t));
+
+    // Create valid items
+    lyric_t lyric[length_lyrics];
+    for (uint32_t i = 0; i < length_lyrics; i++)
+    {
+        lyric[i] = create_lyric(i + 1, god_lyrics[i]);
+    }
+    // Create some noise items
+    lyric_t noise_sentence1 = create_lyric(999, "!T@#1$&*.V,#$D/");
+    lyric_t noise_sentence2 = create_lyric(766, "!H@#I$&E*.,U#$/");
+    lyric_t noise_sentence3 = create_lyric(382, "g!@#g$&w*.,p#$/");
+
+    int idx=0;
+    for(;idx<length_lyrics/2;idx++){
+        send_lyric(perform_set, lyric[idx]);
+    }
+    // send_lyric(perform_set, lyric[0]);
+    // send_lyric(perform_set, lyric[1]);
+    // send_lyric(perform_set, lyric[2]);
+    // send noise
+    send_lyric(perform_set, noise_sentence1);
+    for(;idx<length_lyrics-length_lyrics/6;idx++){
+        send_lyric(perform_set, lyric[idx]);
+    }
+    // send_lyric(perform_set, lyric[3]);
+    // send_lyric(perform_set, lyric[4]);
+     // send noise
+    send_lyric(perform_set, noise_sentence2);
+    for(;idx<length_lyrics;idx++){
+        send_lyric(perform_set, lyric[idx]);
+    }
+    // send_lyric(perform_set, lyric[5]);
+     // send noise
+    send_lyric(perform_set, noise_sentence3);
+
+    while (1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
-void Functional_task(void *pvParameter){
-    Task receive_task;
-    char* function_task = (char*)pvParameter;
-    while(1){
-        if(xQueueReceive(queue, receive_task, pdMS_TO_TICKS(QUEUE_WAITS))== pdTRUE)
+
+// some functional methods
+
+// found a noise item, handle it but not execute, also remove it from queue
+// in this lab, NewJeans will not sing noise sentences
+void removeNoise(void *pvParameter)
+{
+    while (1)
+    {
+        lyric_t self;
+        if (xQueuePeek(perform_set, (void *)&self, (TickType_t)5) == pdTRUE)
+        {
+            if (self.index > __NOISE_VOLUME__)
             {
-                    if (function_task="task1" && receive_task.dataType == 0  )
-                    {
-                        printf("Received from task1 ID= %d\n", receive_task.ID);
-                    }
-                    else if (function_task="task2" && receive_task.dataType == 1  )
-                    {
-                        printf("Received from task2 ID= %d\n", receive_task.ID);
-                    }
-                    else if (receive_task.dataType == 2)
-                    {
-                        printf("Invalid data\n");
-                    }
-                    else xQueueSendToFront(queue, receive_task, QUEUE_SEND_WAITS);
+                ESP_LOGI(TAG, "[Noise] at %ld, Found a NOISE (ID > %d) ", pdTICKS_TO_MS(xTaskGetTickCount()), __NOISE_VOLUME__);
+                ESP_LOGI(TAG, "[Noise] at %ld, Remove noise, ID: %ld, Topic: \"%s\"", pdTICKS_TO_MS(xTaskGetTickCount()), self.index, self.content);
+                xQueueReceive(perform_set, (void *)&self, (TickType_t)5);
+                printf("* NewJeans not sing this noise sentence: %s\n", self.content);
+                printf("\n");
+            }
+            else
+            {
+                // printf("* This is not a noise\n");
             }
         }
-        
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+}
 
-
-
-void app_main(void) 
-{   
-    Task all_task[5]={{1,0},{2,0},{3,0},{4,0},{5,0}};
-    queue=xQueueCreate(QUEUE_SIZE, sizeof(all_task[0]));
-    if (queue == 0)
+// hadle and excuted the item in queue
+// sing a song
+void sing(void *pvParameter)
+{
+    while (1)
     {
-        printf("Failed to create queue= %p\n", queue);
+        lyric_t self;
+        if (xQueuePeek(perform_set, (void *)&self, (TickType_t)5) == pdTRUE)
+        {
+            if (self.index <= __NOISE_VOLUME__)
+            {
+                ESP_LOGI(TAG, "[Receiver] at %ldms, Handle valid item  ", pdTICKS_TO_MS(xTaskGetTickCount()));
+                ESP_LOGI(TAG, "[Receiver] at %ldms, Excuted valid item, ID: %ld, Topic: \"%s\"", pdTICKS_TO_MS(xTaskGetTickCount()), self.index, self.content);
+                xQueueReceive(perform_set, (void *)&self, (TickType_t)5);
+                printf("* NewJeans sing the %ld lyric\n* %s\n", self.index, self.content);
+                printf("\n");
+            }
+            else
+            {
+                // printf("* This is a noise\n");
+            }
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    xTaskCreate(Reception_task, "Reception_task", 4096, all_task, 10, NULL);
-    xTaskCreate(Functional_task, "task1", 4096, NULL, 5, NULL);
-    xTaskCreate(Functional_task, "task2", 4096, NULL, 5, NULL);
+}
 
-    vTaskStartScheduler();
+void app_main(void)
+{
+    set_up();
+    esp_log_level_set(TAG, ESP_LOG_INFO);
+    xTaskCreatePinnedToCore(distribute_lyrics, "distribute_lyrics", 4096, NULL, 5, NULL, 0);
+    xTaskCreatePinnedToCore(removeNoise, "removeNoise", 4096, NULL, 2, NULL, 0);
+    xTaskCreatePinnedToCore(sing, "sing", 4096, NULL, 1, NULL, 0);
 }

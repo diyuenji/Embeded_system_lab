bb5ccb41bab25e30210d0bd051f25b5c65591fc4
diyuenji <112496492+diyuenji@users.noreply.github.com>
2023-12-06 07:41:38 +0700

    lab4

--- Lab4/main/queueConfig.h
+++ Lab4/main/queueConfig.h
@@ -6,11 +6,13 @@
 #define __MAXLEN__ 10
 //[Config available]  If some item has id larger than __NOISE_VOLUME__, it will be labeled as noise
 #define __NOISE_VOLUME__ 100
+
 typedef struct
 {
     uint32_t index;
     char *content;
 } lyric_t;
+
 // this function is create a object
 // In this lab "object" is a lyric
 lyric_t create_lyric(uint32_t input_index, char *input_content);
